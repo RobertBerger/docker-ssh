c3f9933ce2c1d0b5b0daf7deb04eb21a0648b2f4
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-03 16:13:56 +0300

    genius user on host

--- 03_run.sh
+++ 03_run.sh
@@ -2,4 +2,20 @@ echo "+ we assume you already have an ssh server running in your container"
 echo "+ try something like this"
 echo "+ ssh <user>@<container-name.docker> <port>"
 echo "+ e.g.:"
-echo "ssh student@reslocal/ubuntu-base:latest.docker 22"
+echo "ssh genius@reslocal/ubuntu-base:latest.docker 22"
+ret=false
+getent passwd genius >/dev/null 2>&1 && ret=true
+
+if $ret; 
+then
+    echo "genius exists"
+else
+    echo "genius does not exist - create it, log in as genius nd run this again"
+    echo "press <ENTER> to create it"
+    echo "+ sudo useradd genius"
+    sudo useradd genius
+    echo "+ sudo passwd genius"
+    sudo passwd genius
+fi
+
+
